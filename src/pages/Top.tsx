e5157be04691b599d4be2e139ebcbc48ec7977c5
shooh25 <koan6210@outlook.jp>
2024-03-17 20:06:46 +0900

    add: greeting

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { MdEmail } from "react-icons/md";
+import { FaTwitter } from "react-icons/fa6";
+import profile from '../images/profile.png'
 
 const Top = () => {
   return (
@@ -14,7 +16,30 @@ const Top = () => {
       </section>
 
       {/* Greeting */}
-      <section>
+      <section className='px-5 md:px-[50px]'>
+        <div className='border-b border-black relative flex items-end justify-end md:justify-between pb-4'>
+          <img
+            src={profile} alt="kubosyoh"
+            className='absolute w-[120px] md:w-[140px] left-0 top-[-30px] md:left-1/2 md:transform md:-translate-x-1/2' />
+          <p className='hidden md:block text02'>- GREETING</p>
+          <ul className='flex gap-3'>
+            <li>
+              <a href="https://crowdworks.jp/public/employees/4381064" target='_blank' className='bg-black relative h-11 w-11 rounded-[22px] flex justify-center items-center'>
+                <p className='text-white font-rajdhani font-semibold tracking-[.07em]'>CW</p>
+              </a>
+            </li>
+            <li>
+              <a href="" target='_blank' className='bg-black relative h-11 w-11 rounded-[22px] flex justify-center items-center'>
+                <FaTwitter color='#ffffff' className='w-5 h-5' />
+              </a>
+            </li>
+            <li>
+              <a href="mailto:kubosyoh@gmail.com?" className='bg-black relative h-11 w-11 rounded-[22px] flex justify-center items-center'>
+                <MdEmail color='#ffffff' className='w-5 h-5' />
+              </a>
+            </li>
+          </ul>
+        </div>
       </section>
 
       {/* Works */}
@@ -36,7 +61,7 @@ const Top = () => {
       <section>
         <p className='text02'>CONTACT</p>
         <h2 className='text01'>お問い合わせ</h2>
-        
+
       </section>
 
       <section>
