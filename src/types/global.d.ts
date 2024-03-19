9e2c234edb6749fa766b86cd623fd6ff0ef21d6a
shooh25 <koan6210@outlook.jp>
2024-03-19 19:18:36 +0900

    add: types

---
+++ src/types/global.d.ts
@@ -0,0 +1,12 @@
+// microCMS
+// https://document.microcms.io/
+
+interface WorkObj {
+  id: string;
+  title: string;
+  client: string;
+  detail: string;
+  term: string;
+  link: string;
+  scope: string[];
+}
