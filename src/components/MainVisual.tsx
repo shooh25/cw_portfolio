fb2da3cf5494762acf53ce50aa27382d207cb49f
shooh25 <koan6210@outlook.jp>
2024-03-22 23:11:14 +0900

    style: mainVisual

--- src/components/MainVisual.tsx
+++ src/components/MainVisual.tsx
@@ -7,10 +7,12 @@ type Props = {
 
 const MainVisual: React.FC<Props> = ({title, span}) => {
   return (
-    <div className='px-5 md:px-[60px] py-20 md:pt-28 md:pb-20 '>
-      <div className='border-r border-black pr-20 inline-block'>
-        <p className='text02 mb-3'>{span}</p>
-        <h1 className='text01'>{title}</h1>
+    <div className='px-5 md:px-[60px]'>
+      <div className='border-b border-black py-20 md:pt-28 md:pb-20'>
+        <div className='border-r border-black pr-20 inline-block'>
+          <p className='text02 mb-3'>{span}</p>
+          <h1 className='text01'>{title}</h1>
+        </div>
       </div>
     </div>
   )
