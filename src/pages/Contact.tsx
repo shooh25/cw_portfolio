5af3f3e09ca696a450646a8020412e2a759ca46a
shooh25 <koan6210@outlook.jp>
2024-03-30 10:38:09 +0900

    add: contact page layout

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import MainVisual from '../components/MainVisual'
 
-const Contact:React.FC = () => {
+const Contact: React.FC = () => {
   return (
-    <div>Contact</div>
+    <>
+      <MainVisual title='お問い合わせ' span='- CONTACT' />
+      <section className='px-5 md:px-[60px]'>
+        <div className='py-[60px] md:py-[80px]'>
+
+        </div>
+      </section>
+    </>
   )
 }
 
