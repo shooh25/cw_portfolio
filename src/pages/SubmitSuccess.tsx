904dae5c37f6dbf7fd2e7d87f9e5959ca61360a8
shooh25 <koan6210@outlook.jp>
2024-04-06 12:03:04 +0900

    fix: text

--- src/pages/SubmitSuccess.tsx
+++ src/pages/SubmitSuccess.tsx
@@ -9,9 +9,9 @@ const SubmitSuccess: React.FC = () => {
       <>
         <section className='px-5 md:px-[60px]'>
           <div className='py-[60px] md:py-[80px] h-[100vh] relative'>
-            <div className='absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center'>
+            <div className='absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center text-center w-full'>
               <h1 className='text01 mb-6'>送信完了</h1>
-              <p className='text05 text-center mb-11'>
+              <p className='text05  mb-11'>
                 お問い合わせいただき、ありがとうございます。<br />
                 近日中にあらためてご連絡いたします。
               </p>
